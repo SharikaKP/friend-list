69930b91f88c8f6263c78644b045dee2563323a9
Sharika Krishnaprasad <sharika.krishnaprasad@envestnetrs.com>
2021-03-27 15:25:23 +0530

    Version_2

--- src/components/FriendList/index.js
+++ src/components/FriendList/index.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import "./styles.css";
 
 const FriendList = () => {
@@ -15,30 +16,68 @@ const FriendList = () => {
     { id: 3, name: "Athira" },
   ]);
   const [newName, setNewName] = useState();
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    event.target.reset();
-    setList([...list, { id: list.length, name: newName }]);
+  const onSubmit = (event) => {
+    if (event.charCode === 13) {
+      event.preventDefault();
+      setNewName('');
+      setList([...list, { id: list.length, name: newName }]);
+    }
   };
+  const [search, setSearch] = useState("");
+  const onDelete = (id) => {
+    setList(list.filter((item) => item.id != id));
+  };
+  const onSearchChange = (value) => {
+    setSearch(value);
+  };
+
+  const listData = useMemo(() => {
+    let modifiedList = list;
+    if (search) {
+      modifiedList = modifiedList.filter((item) =>
+        item.name.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+    return modifiedList;
+  }, [list, search]);
+
   return (
-    <Card class="card">
-      <Card.Header class="card-header">
+    <Card className="card">
+      <Card.Header className="card-header">
         <strong>Friends List</strong>
       </Card.Header>
       <ListGroup variant="flush">
-        <Form onSubmit={handleSubmit}>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Control
+                placeholder="Search"
+                onChange={(e) => onSearchChange(e.target.value)}
+              />
+            </Col>
+          </Row>
           <Row>
             <Col>
               <Form.Control
                 onChange={(event) => setNewName(event.target.value)}
                 placeholder="Enter your friend's name"
+                onKeyPress={onSubmit}
+                value={newName}
               />
             </Col>
           </Row>
         </Form>
-        {list.length > 0 &&
-          list.map((item) => (
-            <ListGroup.Item key={item.id}>{item.name}</ListGroup.Item>
+        {listData.length > 0 &&
+          listData.map((item) => (
+            <ListGroup.Item key={item.id}>
+              <div className="list-item">
+                <strong>{item.name}</strong>
+                <div>is your friend</div>
+              </div>
+              <span className="icon" onClick={() => onDelete(item.id)}>
+                <FontAwesomeIcon icon={faTrash} />
+              </span>
+            </ListGroup.Item>
           ))}
       </ListGroup>
     </Card>
